667abe8496b36a4fb34efe6c20775f7857fe8594
Ansar Karzhaspayev <ansarkarzh@172.28.216.240-red.dhcp.yndx.net>
2025-03-06 18:56:25 +0300

    temp

---
+++ users_service/app/models.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String, Date, DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    email = Column(String, unique=True, nullable=False)
+    first_name = Column(String)
+    last_name = Column(String)
+    birth_date = Column(Date)
+    phone_number = Column(String)
+    created_at = Column(DateTime, server_default=func.now())
+    updated_at = Column(DateTime, server_default=func.now(), onupdate=func.now())
